5d176b06d522ed235ed6c9c868b6c0020d746309
yufimtsev <yufimtsev@yandex-team.ru>
2017-02-25 00:18:56 +0500

    Final delay fixed

--- blitz/src/main/java/com/yufimtsev/tenhou/clouds/blitz/model/Constants.java
+++ blitz/src/main/java/com/yufimtsev/tenhou/clouds/blitz/model/Constants.java
@@ -25,5 +25,6 @@ public class Constants {
             TimeUnit.MINUTES.toMillis(1),
             TimeUnit.MINUTES.toMillis(1),
             TimeUnit.MINUTES.toMillis(1),
+            TimeUnit.MINUTES.toMillis(1),
     };
 }
